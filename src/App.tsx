fd945a9ef3fcb1ec1d3073018d65e648ba6691c7
SsongCh94 <thddkwl94@gmail.com>
2023-04-06 21:52:51 +0900

    [REMOVE] : ✂️ Remove TestCode

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-// import React from 'react';
-
-// function App() {
-//   return <div>App</div>;
-// }
-
-// export default App;
-
 import React from 'react';
-import Feed from './components/Feed/Feed';
 
 function App() {
-  return <Feed />;
+  return <div>App</div>;
 }
 
 export default App;
