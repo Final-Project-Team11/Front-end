21bdafc18869e2bbfbf66d8a91cd487dd1be549a
choidami5126 <dahyun5003@kakao.com>
2023-04-07 18:12:45 +0900

    [FIX] : 🐛 Style 키를 명시해주지 않았던 부분 수정

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import Modal from '../../components/Modal/Modal';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [open, setOepn] = React.useState(false);
+  const openModal = () => {
+    setOepn(true);
+  };
+  const closeModal = () => {
+    setOepn(false);
+  };
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          openModal();
+        }}
+      >
+        테스트
+      </button>
+      {open && (
+        <Modal size="small" closeModal={closeModal}>
+          테스트
+        </Modal>
+      )}
+    </>
+  );
 };
 
 export default Login;
