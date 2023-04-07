b6a8177969e972d8b7b9d1b4671640a2a1a04d1b
choidami5126 <dahyun5003@kakao.com>
2023-04-07 18:11:02 +0900

    [FEAT] : ⚙️ tanstackQuery 기본 틀 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import Router from './shared/Router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    },
+  },
+});
 
 function App() {
-  return <Router />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
---
+++ src/api/utils/createQueryKey.ts
@@ -0,0 +1,7 @@
+export const keys = {
+  // 예시
+  // GET_POSTS: ["GET_POSTS"],
+  // GET_POST_ONE: ["GET_POST_ONE"],
+  // GET_USERNAME: ["GET_USENAME"],
+  // GET_MYPOST: ["GET_MYPOST"],
+};
