b573e909bc57afa0387a2ffb7e5aec2579db5217
SsongCh94 <thddkwl94@gmail.com>
2023-04-08 13:32:09 +0900

    [DESIGN] : 🎨 Uploaded 파일 기본 틀 잡기

---
+++ src/components/Uploaded/UploadedFile.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StDeviderBlock, StUploadedBlock } from './style';
+
+const UploadedFile = () => {
+  return (
+    <StUploadedBlock>
+      🗂️
+      <StDeviderBlock />
+    </StUploadedBlock>
+  );
+};
+
+export default UploadedFile;
