89b5dd33628c6f1035023802532caadac0457a40
SsongCh94 <thddkwl94@gmail.com>
2023-04-08 11:00:33 +0900

    [FEAT] : ⚙️ Add Axios interceptor

--- src/axios/api.tsx
+++ src/axios/api.tsx
@@ -6,4 +6,31 @@ const config: AxiosRequestConfig = {
 
 const instnace: AxiosInstance = axios.create(config);
 
+instnace.interceptors.request.use(
+  // 요청을 보내기 전 수행되는 함수
+  function (config) {
+    // const token = cookies.get("token")
+    // config.headers["Authorization"] = `${token}`;
+    return config;
+  },
+
+  // 오류 요청을 보내기 전 수행되는 함수
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+instnace.interceptors.response.use(
+  // 응답을 내보내기 전 수행되는 함수
+  function (response) {
+    return response;
+  },
+
+  // 오류 응답을 내보내기 전 수행되는 함수
+  function (error) {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
 export default instnace;
