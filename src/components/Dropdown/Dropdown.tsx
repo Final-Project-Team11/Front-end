793086ef3bac1015bcd965e429a0ada09a098ec1
choidami5126 <dahyun5003@kakao.com>
2023-04-11 05:36:27 +0900

    [FEAT] : ⚙️ Custom Common Dropdown-Component add

---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { DropdownProps } from './interfaces'
+import { StUl, StLi } from './styles'
+import { IoIosArrowDown, IoIosArrowUp  } from "react-icons/io";
+
+const Dropdown : React.FC<DropdownProps> = ({items, children, size, color, background, border}) => {
+  const [isOpen, setIsOpen] = React.useState(false)
+  const [selectedItem, setSelectedItem] = React.useState('')
+  // 선택된 아이템을 저장하는 상태 값
+
+  const clickItemHandler = (item : string) => {
+    setSelectedItem(item)
+    setIsOpen(false)
+  }
+
+  return (
+    <>
+      <StUl onClick={() => setIsOpen(!isOpen)} size={size} color={color} background={background} border={border}>
+        {children}{'  '}{isOpen ? <IoIosArrowUp/> : <IoIosArrowDown/>}
+      </StUl>
+      {isOpen && (
+        <StUl size={size} color={color} background={background} border={border}>
+          {items.map((item : string) => (
+            <StLi key={item} onClick={()=> clickItemHandler(item)} size={size} color={color} background={background} border={border}>{item}</StLi>
+          ))}
+        </StUl>
+      )}
+    </>
+  )
+}
+
+export default Dropdown
