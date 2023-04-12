844aed69569154ec3a80062ff9934ea95f407642
ChanwooPark <chanw9503@gmail.com>
2023-04-12 20:32:22 +0900

    [FIX] : 🐛  queryKey 수정 tab의 변화가 있을 때마다, api호출을 할 수 있도록 변경

--- src/api/hooks/useGetMain.ts
+++ src/api/hooks/useGetMain.ts
@@ -3,15 +3,21 @@ import React from 'react';
 import { keys } from '../utils/createQueryKey';
 import api from '../../axios/api';
 
-const useGetMain = () => {
+const useGetMain = (type: number) => {
   const { data, isLoading } = useQuery({
-    queryKey: keys.GET_MAIN,
+    queryKey: [keys.GET_MAIN, type],
     queryFn: async () => {
-      const data = await api.get('/main');
-      return data.data;
+      if (type === 0) {
+        const data = await api.get('/totalSchedule');
+        console.log('schedule', data);
+        return data.data.main;
+      } else if (type === 1) {
+        console.log('vacation');
+        const data = await api.get('/totalVacation');
+        return data.data.main;
+      }
     },
   });
-
   return { data, isLoading };
 };
 
