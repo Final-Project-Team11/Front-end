de04dbb96b11c7d27b557afc6e40277a5a9a5253
SsongCh94 <thddkwl94@gmail.com>
2023-04-12 12:34:08 +0900

    [FEAT] : ⚙️ AddCategory 렌더링 시 focus

--- src/components/Feed/Category/AddCategory.tsx
+++ src/components/Feed/Category/AddCategory.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { StCategoryInputBlock, StCategoryInput } from './style';
 import { StCircleBlock } from '../style';
 import { AddCategoryProps } from './interfaces';
 
 const AddCategory = ({ value, onChange, setValue }: AddCategoryProps) => {
+  // AddCategory 인풋 생기면 focus
+  const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  });
+
   return (
     <StCategoryInputBlock>
       <StCircleBlock />
-      <StCategoryInput type="text" maxLength={10} value={value} onChange={onChange} />
+      <StCategoryInput
+        ref={inputRef}
+        type="text"
+        maxLength={10}
+        value={value}
+        onChange={onChange}
+      />
     </StCategoryInputBlock>
   );
 };
