376f218bd47b81893fd9eb551ac3b776b64ed9fd
ChanwooPark <chanw9503@gmail.com>
2023-04-12 20:29:27 +0900

    [DESIGN] : 🎨 GlobalStyles 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Router from './shared/Router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import GlobalStyle from './shared/GlobalStyle';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -13,6 +14,7 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <GlobalStyle />
       <Router />
     </QueryClientProvider>
   );
