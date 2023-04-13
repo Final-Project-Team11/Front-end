ed32c86b3403dfe64699f2d77ee576b7c4febc9e
ChanwooPark <chanw9503@gmail.com>
2023-04-13 16:12:10 +0900

    [FEAT] : ⚙️ Main Page API hooks setting

--- src/api/hooks/usePostschedule.ts
+++ src/api/hooks/usePostschedule.ts
@@ -1,26 +1,70 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import api from '../../axios/api';
 import React from 'react';
+import { keys } from '../utils/createQueryKey';
 
 interface Paylaod {
-  startDay?: Date;
-  endDay?: Date;
-  title: string;
-  location?: string;
-  content?: string;
-  ref?: string; //멘션
-  file?: string;
-  typeDetail?: string; //휴가 종류 or 일정 종류
-  type?: number; //일정,휴가....
-  result?: string;
+  schedule?: {
+    startDay?: Date;
+    endDay?: Date;
+    title: string;
+    location?: string;
+    content?: string;
+    ref?: string; //멘션
+    file?: string;
+  };
+
+  other?: {
+    startDay?: Date;
+    endDay?: Date;
+    title: string;
+    content?: string;
+    ref?: string; //멘션
+    file?: string;
+  };
+
+  meeting?: {
+    startDay?: Date;
+    endDay?: Date;
+    title: string;
+    location?: string;
+    content?: string;
+    ref?: string; //멘션
+    file?: string;
+  };
+
+  report?: {
+    title: string;
+    content?: string;
+    ref?: string; //멘션
+    file?: string;
+  };
+
+  vacation?: {
+    typeDetail?: string; //휴가 종류 or 일정 종류
+    startDay?: Date;
+    endDay?: Date;
+  };
+
+  url: string;
 }
 
 const usePostschedule = () => {
+  const queryClient = useQueryClient();
+
   const mutation = useMutation({
     mutationFn: async (payload: Paylaod) => {
-      const data = api.post(`${payload.result}`);
+      const data = api.post(`/${payload.url}`, payload.vacation);
+      return data;
+    },
+
+    onSuccess: () => {
+      console.log('success');
+      queryClient.invalidateQueries([keys.GET_MAIN, 1]);
     },
   });
+
+  return mutation;
 };
 
 export default usePostschedule;
