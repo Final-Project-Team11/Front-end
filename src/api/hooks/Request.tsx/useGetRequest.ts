702f704c24cdc014bb482051b9034affaec941d1
SsongCh94 <thddkwl94@gmail.com>
2023-04-16 02:41:53 +0900

    [FEAT] : ⚙️ 무한스크롤, GET 요청 완료

---
+++ src/api/hooks/Request.tsx/useGetRequest.ts
@@ -0,0 +1,42 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import instnace from '../../../axios/api';
+import { keys } from '../../utils/createQueryKey';
+import { AxiosError } from 'axios';
+
+interface File {
+  eventId: number;
+  userName: string;
+  title: string;
+  file: string;
+  startDay: string;
+  endDay: string;
+  status: 'submit' | 'accept' | 'deny';
+}
+
+interface PageData {
+  schedule: File[];
+  pageNum: number;
+}
+
+export const useGetRequest = () => {
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery<
+    PageData,
+    AxiosError,
+    PageData
+  >({
+    queryKey: [keys.GET_REQUEST_LIST], // 쿼리 키
+    queryFn: async ({ pageParam = 1 }) => {
+      const response = await instnace.get(`/schedule?size=5&page=${pageParam}`);
+      return { ...response.data, pageNum: pageParam };
+    }, // API 호출 함수
+    getNextPageParam: lastPage => {
+      // 다음 페이지를 가져오는 로직입니다. lastPage에서 pageNum을 추출하여 1 증가시킵니다.
+      const nextPageNum = lastPage.pageNum + 1;
+
+      // 만약 lastPage의 데이터가 비어 있다면, 'undefined'를 반환하여 다음 페이지를 요청하지 않도록 합니다.
+      return lastPage.schedule && lastPage.schedule.length > 0 ? nextPageNum : undefined;
+    },
+  });
+
+  return { data, fetchNextPage, hasNextPage };
+};
