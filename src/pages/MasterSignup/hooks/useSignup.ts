67f3aa7cb9a42eba44272008f9c55f25b2193e9a
choidami5126 <dahyun5003@kakao.com>
2023-04-18 23:16:37 +0900

    [RENAME]  : ✏️ 직관적인 타입명으로 수정

--- src/pages/MasterSignup/hooks/useSignup.ts
+++ src/pages/MasterSignup/hooks/useSignup.ts
@@ -1,12 +1,12 @@
 import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
-import { SignupInfo } from '../interfaces';
+import { AdminSignupInfo } from '../interfaces';
 import instnace from '../../../axios/api';
 
 export const useSignup = () => {
   const navigate = useNavigate();
   const signup = useMutation(
-    async (item: SignupInfo) => {
+    async (item: AdminSignupInfo) => {
       console.log('아이템', item);
       const data = await instnace.post(`/signUp`, item);
       return data;
