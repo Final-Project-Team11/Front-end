0d58695635f555db6117d7f6a4aec6870ffb169b
SsongCh94 <thddkwl94@gmail.com>
2023-04-19 03:57:10 +0900

    [DESIGN] : 🎨 마이페이지 틀 잡기

--- src/pages/MyPage/styles.ts
+++ src/pages/MyPage/styles.ts
@@ -1,1 +1,64 @@
 import styled from 'styled-components';
+
+export const Frame = styled.div`
+  width: 1920px;
+  height: 1080px;
+
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+export const Wrapper = styled.div`
+  width: 1200px;
+  height: 100%;
+  padding-top: 40px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  gap: 31px;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 1100px;
+  height: fit-content;
+`;
+
+export const Calendar = styled.div`
+  background-color: aliceblue;
+  width: 823px;
+  height: 113px;
+`;
+
+export const MainArea = styled.div`
+  width: 1098px;
+  height: 748px;
+
+  display: flex;
+  gap: 28px;
+`;
+
+export const TabArea = styled.div`
+  width: 100%;
+  height: 100%;
+
+  margin-top: 14px;
+
+  display: flex;
+  gap: 20px;
+`;
+
+export const MiddleArea = styled.div`
+  width: 260px;
+  height: 734px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
