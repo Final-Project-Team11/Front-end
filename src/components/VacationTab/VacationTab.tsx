d93826ef321addac948a367403c6777add020c57
SsongCh94 <thddkwl94@gmail.com>
2023-04-18 14:05:05 +0900

    [DESIGN] : 🎨 vacation-board 스타일 통합

--- src/components/VacationTab/VacationTab.tsx
+++ src/components/VacationTab/VacationTab.tsx
@@ -3,6 +3,7 @@ import * as UI from './style';
 import Vacation from './Vacation/Vacation';
 import { useGetVacation } from '../../api/hooks/Vacation/useGetVacation';
 import { VacationList } from './interfaces';
+import Board from '../Board/Board';
 
 const VacationTab = () => {
   // Vacation 리스트 GET 요청
@@ -48,25 +49,23 @@ const VacationTab = () => {
     return <h1>....loading</h1>;
   }
 
+  const icon = (
+    <svg viewBox="0 0 146.01 117.08" width="20px" height="20px">
+      <path
+        fill="#87acd7"
+        d="M131.2,15.46h-58.65S58.09,0,58.25,0H14.29C6.4,0,0,6.4,0,14.29L.16,102.27c0,8.18,6.63,14.81,14.81,14.81h116.22c8.18,0,14.81-6.63,14.81-14.81V30.27c0-8.18-6.63-14.81-14.81-14.81Z"
+      />
+    </svg>
+  );
+
   return (
-    <UI.StTabBlock>
-      <UI.StIconBlock>
-        {/* 아이콘 */}
-        <svg viewBox="0 0 146.01 117.08" width="20px" height="20px">
-          <path
-            fill="#87acd7"
-            d="M131.2,15.46h-58.65S58.09,0,58.25,0H14.29C6.4,0,0,6.4,0,14.29L.16,102.27c0,8.18,6.63,14.81,14.81,14.81h116.22c8.18,0,14.81-6.63,14.81-14.81V30.27c0-8.18-6.63-14.81-14.81-14.81Z"
-          />
-        </svg>
-        <UI.StTitleSpan>휴가 요청</UI.StTitleSpan>
-      </UI.StIconBlock>
-      <UI.StDivederHr />
+    <Board icon={icon} title="휴가 요청">
       <UI.StInsideBlock ref={targetDiv}>
         {vacations?.map((vacation: VacationList) => {
           return <Vacation key={vacation.eventId} vacation={vacation} />;
         })}
       </UI.StInsideBlock>
-    </UI.StTabBlock>
+    </Board>
   );
 };
 
