889f78a06f65c3702efff10191427f8df26676b5
ChanwooPark <chanw9503@gmail.com>
2023-04-18 11:56:01 +0900

    [FEAT] : ⚙️ 팀조회 querykey 추가

--- src/api/utils/createQueryKey.ts
+++ src/api/utils/createQueryKey.ts
@@ -2,8 +2,8 @@ export const keys = {
   // 예시
 
   GET_MAIN: 'GET_MAIN',
-
   GET_POSTS: 'GET_POSTS',
+  GET_TEAM_INFO: 'GET_TEAM_INFO',
   // GET_POST_ONE: ["GET_POST_ONE"],
   // GET_USERNAME: ["GET_USENAME"],
   // GET_MYPOST: ["GET_MYPOST"],
