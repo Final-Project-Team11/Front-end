3bff00681477436ca4753cb2553f137d0e10ebf6
choidami5126 <dahyun5003@kakao.com>
2023-04-18 23:34:38 +0900

    [FEAT] : ⚙️ 유저 생성에 필요한 인터페이스를 구현하였습니다.

---
+++ src/pages/Business/interfaces.ts
@@ -0,0 +1,10 @@
+export interface UserSignupInfo {
+  userName: string;
+  team: string;
+  rank: string;
+  job: string;
+  userId: string;
+  joinDay: Date;
+  salaryDay: number;
+  authLevel: number;
+}
