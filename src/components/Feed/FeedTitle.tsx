30c326c2222b44da1370174cddae04368acbf54a
SsongCh94 <thddkwl94@gmail.com>
2023-04-18 19:56:10 +0900

    [DESIGN] : 🎨 feed style 변경중

--- src/components/Feed/FeedTitle.tsx
+++ src/components/Feed/FeedTitle.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-
 import { FeedTitleProps } from './interfaces';
 import * as UI from './style';
 
 const FeedTitle = ({ onClick }: FeedTitleProps) => {
   return (
     <UI.StFeedTitleBlock>
-      <UI.StFeedTitleH1>feed</UI.StFeedTitleH1>
-      <UI.StPlusBlock onClick={onClick}>+</UI.StPlusBlock>
+      <UI.StFeedTitleH1>todo</UI.StFeedTitleH1>
+      <UI.StPlusSpan onClick={onClick}>+</UI.StPlusSpan>
     </UI.StFeedTitleBlock>
   );
 };
