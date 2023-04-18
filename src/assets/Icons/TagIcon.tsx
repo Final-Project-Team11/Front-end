007bcdf820b4b32652a0b8198c9b95dc21992390
ChanwooPark <chanw9503@gmail.com>
2023-04-19 01:51:14 +0900

    [DESIGN] : 🎨 Icon fill props add

--- src/assets/Icons/TagIcon.tsx
+++ src/assets/Icons/TagIcon.tsx
@@ -14,7 +14,7 @@ const TagIcon = (props: IconProps) => {
       viewBox="0 0 54 48"
       preserveAspectRatio="xMidYMid meet"
     >
-      <g fill="none" fillRule="evenodd">
+      <g fill={props.fill ? props.fill : '#DDDDDD'} fillRule="evenodd">
         <path d="M47.59,0S17.34,.01,17.3,.01c-1.2,.03-2.28,.48-3.13,1.21C13.83,1.51,.7,15.87,.7,15.87c-.94,1.02-.94,2.58,0,3.6,0,0,13.13,14.36,13.46,14.65,.85,.73,1.93,1.19,3.13,1.21,.04,0,30.29,.01,30.29,.01,2.76,0,4.99-2.24,4.99-4.99V4.99c0-2.76-2.24-4.99-4.99-4.99Z" />
       </g>
     </svg>
