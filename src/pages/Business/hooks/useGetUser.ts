08861a61477cacab149f99a35036b5511536eb4e
choidami5126 <dahyun5003@kakao.com>
2023-04-20 13:40:24 +0900

    [FEAT] : ⚙️ GET User hook add

---
+++ src/pages/Business/hooks/useGetUser.ts
@@ -0,0 +1,32 @@
+import { useQuery } from '@tanstack/react-query';
+import apis from '../../../api/axios/api';
+import { keys } from '../../../api/utils/createQueryKey';
+
+export interface Users {
+  userId: string;
+  userName: string;
+  team: string;
+  rank: string;
+  joinDay: Date | null;
+  job: string;
+  salaryDay: number;
+  authLevel: string;
+}
+
+interface Test {
+  //   data: AxiosResponse | undefined;
+  data: Users[];
+  isLoading: boolean;
+}
+
+export const useGetUser = (): Test => {
+  const { data, isLoading } = useQuery({
+    queryKey: [keys.GET_USER_LIST],
+    queryFn: async () => {
+      const response = await apis.get('/users');
+      return response.data.users;
+    },
+  });
+
+  return { data, isLoading };
+};
