3ee73b2469f09e1dcbf5fc71aaa5da95d5fc46bc
ChanwooPark <chanw9503@gmail.com>
2023-04-20 22:34:12 +0900

    [FIX] : 🐛 File Upload bug fix - File Upload 시, FormData 형식으로 올려야한다.   또한 body에 보낼 수 있는 정보는 모두 FormData 여야 한다. 그래야 Content-Type: 을 multiPart/form-data 형식으로 맞출 수 있다.

--- src/api/hooks/Main/usePostschedule.ts
+++ src/api/hooks/Main/usePostschedule.ts
@@ -10,28 +10,27 @@ interface Paylaod {
     title?: string;
     location?: string;
     content?: string;
-    ref?: string; //멘션
-    file?: string;
+    ref?: string[]; //멘션
+    file?: File | string;
+    eventType?: string;
+    startTime?: string;
   };
   url: string;
 }
 
 const usePostschedule = () => {
   const queryClient = useQueryClient();
-
   const mutation = useMutation({
     mutationFn: async (payload: Paylaod) => {
-      if (payload.postInfo?.file) {
-        const newPostInfo = {
-          ...payload.postInfo,
-          file: JSON.parse(payload.postInfo.file),
-        };
-        const data = api.post(`/${payload.url}`, newPostInfo);
-        return data;
-      } else {
-        const data = api.post(`/${payload.url}`, payload.postInfo);
-        return data;
-      }
+      console.log(payload);
+
+      const formData = getFormData(payload);
+      const data = await api.post(`/${payload.url}`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      return data;
     },
 
     onSuccess: () => {
@@ -45,3 +44,53 @@ const usePostschedule = () => {
 };
 
 export default usePostschedule;
+
+const getFormData = (payload: Paylaod): FormData | undefined => {
+  switch (payload.url) {
+    case 'meeting': {
+      const formData = new FormData();
+      formData.append('file', payload.postInfo?.file || '');
+      formData.append('startDay', payload.postInfo?.startDay || '');
+      formData.append('startTime', payload.postInfo?.startTime || '');
+      formData.append('title', payload.postInfo?.title || '');
+      formData.append('content', payload.postInfo?.content || '');
+      formData.append('file', payload.postInfo?.location || '');
+      payload.postInfo?.ref?.map((item, index) => formData.append(`ref[${index}]`, item));
+      formData.append('eventType', payload.postInfo?.eventType || '');
+      formData.append('location', payload.postInfo?.location || '');
+      return formData;
+    }
+
+    case 'report': {
+      const formData = new FormData();
+      formData.append('title', payload.postInfo?.title || '');
+      formData.append('content', payload.postInfo?.content || '');
+      payload.postInfo?.ref?.map((item, index) => formData.append(`ref[${index}]`, item));
+      formData.append('file', payload.postInfo?.file || '');
+      formData.append('enrollDay', payload.postInfo?.startDay || '');
+      return formData;
+    }
+
+    case 'other': {
+      const formData = new FormData();
+      formData.append('startDay', payload.postInfo?.startDay || '');
+      formData.append('endDay', payload.postInfo?.endDay || '');
+      formData.append('title', payload.postInfo?.title || '');
+      formData.append('content', payload.postInfo?.content || '');
+      payload.postInfo?.ref?.map((item, index) => formData.append(`ref[${index}]`, item));
+      formData.append('file', payload.postInfo?.file || '');
+      return formData;
+    }
+
+    case 'vacation': {
+      const formData = new FormData();
+      formData.append('startDay', payload.postInfo?.startDay || '');
+      formData.append('endDay', payload.postInfo?.endDay || '');
+      formData.append('typeDetail', payload.postInfo?.eventType || '');
+      return formData;
+    }
+
+    default:
+      return undefined;
+  }
+};
