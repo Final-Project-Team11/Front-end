c466c337fb5da5c442d9985f1351621f25867671
choidami5126 <dahyun5003@kakao.com>
2023-04-22 01:25:28 +0900

    [FIX] : 🔧 navigate 설정, 이미지 오타 수정, 헤더 사이즈 수정

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import logo from '../../assets/images/home/logo.png';
 import main1 from '../../assets/images/home/main1.jpg';
 import main2 from '../../assets/images/home/main2.jpg';
@@ -6,11 +7,13 @@ import main4 from '../../assets/images/home/main4.jpg';
 import styled from 'styled-components';
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <StBlock>
       <Header style={{ background: 'white' }}>
         <img src={logo} />
-        <StButton>시작하기</StButton>
+        <StButton onClick={() => navigate(`/login`)}>시작하기</StButton>
       </Header>
 
       <StImg src={main1} />
@@ -37,7 +40,7 @@ const StImg = styled.img`
 
 const Header = styled.div`
   width: 100%;
-  height: 150px;
+  height: 120px;
   display: flex;
   justify-content: space-between;
 
@@ -48,15 +51,17 @@ const Header = styled.div`
   position: fixed;
   top: 0;
   z-index: 100;
+
+  box-shadow: 0px 2px 2px 0px rgba(0, 0, 0, 0.1);
 `;
 
 const StButton = styled.button`
-  width: 240px;
-  height: 90px;
+  width: 120px;
+  height: 60px;
   border-radius: 45px;
   background-color: #e64042;
   border: none;
   font-weight: bold;
-  font-size: 30px;
+  font-size: 20px;
   color: white;
 `;
