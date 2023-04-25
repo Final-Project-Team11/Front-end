d59220481b7011650eb3f6494f7a9d9460fc0a69
ChanwooPark <chanw9503@gmail.com>
2023-04-25 22:08:22 +0900

    [REFACTOR] : ♻️ 1주일 캘린더로 변경하기

--- src/components/Main/CustomCalendar/CustomCalendar.tsx
+++ src/components/Main/CustomCalendar/CustomCalendar.tsx
@@ -1,20 +1,21 @@
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { ISchedule } from './interfaces';
 import {
-  Styear,
-  StMonth,
-  StTitleDateBlock,
   StWeek,
-  StWeekdayBlock,
   StContainer,
-  StTitle,
-  Stpagination,
   StWeekBlock,
   StDateBlcok,
+  StLine,
+  StEventBlock,
+  StEventContainer,
 } from './styles';
 import Weekday from './Weekday';
 
-const CustomCalendar = () => {
+interface CalendarProps {
+  width: string;
+}
+
+const CustomCalendar = (props: CalendarProps) => {
   const today = {
     year: new Date().getFullYear(),
     month: new Date().getMonth() + 1,
@@ -22,8 +23,21 @@ const CustomCalendar = () => {
     day: new Date().getDay(),
   };
 
+  const width = props.width.split('px')[0];
+
   const start = new Date();
-  const end = new Date(new Date().setDate(start.getDate() + 5));
+  const start1 = new Date(new Date().setDate(start.getDate() + 1));
+  const start2 = new Date(new Date().setDate(start.getDate() + 2));
+  const start3 = new Date(new Date().setDate(start.getDate() + 3));
+  const start4 = new Date(new Date().setDate(start.getDate() + 4));
+
+  const end = new Date(new Date().setDate(start.getDate() + 1));
+  const end1 = new Date(new Date().setDate(start1.getDate() + 1));
+  const end2 = new Date(new Date().setDate(start2.getDate() + 1));
+  const end3 = new Date(new Date().setDate(start3.getDate() + 1));
+  const end4 = new Date(new Date().setDate(start4.getDate() + 1));
+
+  const widthPercent = (100 / 7).toString() + '%';
 
   const schedules: ISchedule[] = [
     {
@@ -31,13 +45,34 @@ const CustomCalendar = () => {
       userName: '찬우',
       startDay: start,
       endDay: end,
+      title: 'textsadadasdasdasdasdadasdasdsadsadas',
+    },
+    {
+      eventId: 0,
+      userName: '찬우',
+      startDay: start1,
+      endDay: end1,
       title: 'text',
     },
     {
       eventId: 0,
       userName: '찬우',
-      startDay: start,
-      endDay: end,
+      startDay: start2,
+      endDay: end2,
+      title: 'text',
+    },
+    {
+      eventId: 0,
+      userName: '찬우',
+      startDay: start3,
+      endDay: end3,
+      title: 'text',
+    },
+    {
+      eventId: 0,
+      userName: '찬우',
+      startDay: start4,
+      endDay: end4,
       title: 'text',
     },
   ];
@@ -45,84 +80,28 @@ const CustomCalendar = () => {
   const week = ['SUN', 'MON', 'TUE', 'WEN', 'THU', 'FRI', 'SAT']; //일주일
   const [selectedYear, setSelectedYear] = useState(today.year); //현재 선택된 연도
   const [selectedMonth, setSelectedMonth] = useState(today.month); // 현재 선택된 달
+  const [selectedDate, setSelectedDate] = useState(today.date);
 
   const dateTotalCount = new Date(selectedYear, selectedMonth, 0).getDate(); //선택된 연도, 달의 마지막 날짜
 
   //이전달 선택
-  const preMonth = useCallback(() => {
-    //이전 달 보기 보튼
-    if (selectedMonth == 1) {
-      setSelectedMonth(12);
-      setSelectedYear(selectedYear - 12);
-    } else {
-      setSelectedMonth(selectedMonth - 1);
-    }
-  }, [selectedMonth]);
-
-  const nextMonth = useCallback(() => {
-    //다음달 보기 버튼
-    if (selectedMonth == 12) {
-      setSelectedMonth(1);
-      setSelectedYear(selectedYear + 1);
-    } else {
-      setSelectedMonth(selectedMonth + 1);
-    }
-  }, [selectedMonth]);
-
-  const monthControl = useCallback(() => {
-    //달 선택 박스 고르기
-
-    const monthArr = [];
-    for (let i = 0; i < 12; i++) {
-      monthArr.push(
-        <option key={i + 1} value={i + 1}>
-          {i + 1} 월
-        </option>
-      );
-    }
-
-    return (
-      <select value={selectedMonth} onChange={changeSelectMonth}>
-        {monthArr}
-      </select>
-    );
-  }, [selectedMonth]);
-
-  const changeSelectMonth = (e: any) => {
-    setSelectedMonth(Number(e.target.value));
-  };
-  const changeSelectYear = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedYear(Number(e.target.value));
-    console.log(Number(e.target.value));
-  };
-
-  const yearControl = useCallback(() => {
-    //연도 선택박스에서 고르기
-    const yearArr = [];
-    const startYear = today.year - 10; //현재 년도부터 10년전 까지만
-    const endYear = today.year + 10; //현재 년도부터 10년 후까지만
-
-    for (let i = startYear + 1; i < endYear + 1; i++) {
-      yearArr.push(
-        <option key={i} value={i}>
-          {i}
-        </option>
-      );
-    }
-    return (
-      <select className="yearSelect" onChange={changeSelectYear} value={selectedYear}>
-        {yearArr}
-      </select>
-    );
-  }, [selectedYear]);
 
   const returnWeek = useCallback(() => {
     //요일 반환 함수
     const weekArr: any[] = [];
 
-    week.forEach(v => {
-      weekArr.push(<StWeek key={v}>{v}</StWeek>);
-    });
+    for (const nowDay of week) {
+      const day = new Date(selectedYear, selectedMonth - 1, today.date).getDay();
+      if (week[day] == nowDay) {
+        for (let i = 0; i < 7; i++) {
+          weekArr.push(
+            <StWeek key={i} width={widthPercent}>
+              {week[(day + i) % 7]}
+            </StWeek>
+          );
+        }
+      }
+    }
 
     return weekArr;
   }, []);
@@ -134,38 +113,77 @@ const CustomCalendar = () => {
     for (const nowDay of week) {
       const day = new Date(selectedYear, selectedMonth - 1, 1).getDay();
       if (week[day] == nowDay) {
-        for (let i = 0; i < dateTotalCount; i++) {
-          dayArr.push(<Weekday id={i} schedules={schedules} />);
+        for (let i = 0; i < 7; i++) {
+          if (today.date + i > dateTotalCount) {
+            dayArr.push(
+              <Weekday id={today.date + i - dateTotalCount} width={widthPercent} />
+            );
+          } else {
+            dayArr.push(<Weekday id={today.date + i} width={widthPercent} />);
+          }
         }
-      } else {
-        dayArr.push(<StWeekdayBlock></StWeekdayBlock>);
       }
     }
 
     return dayArr;
   }, [selectedYear, selectedMonth, dateTotalCount]);
 
+  const returnEvent = useCallback(() => {
+    const CountArr = Array.from({ length: 31 }, () => [false, false, false]);
+
+    console.log('CountArr', CountArr);
+
+    const eventArr = schedules.map(item => {
+      const blockCount = item.endDay.getDate() - item.startDay.getDate();
+      const leftCount = item.startDay.getDate() - selectedDate;
+      const leftPercent = (100 / 7) * leftCount + '%';
+      const widthPercent = (100 / 7) * (blockCount + 1) + '%';
+
+      let tempValue = 0;
+
+      for (let j = 0; j < 3; j++) {
+        if (CountArr[Number(item.startDay.getDate())][j] === false) {
+          tempValue = j + 1;
+          break;
+        }
+      }
+
+      const topPercent = (100 / 4) * tempValue + '%';
+
+      for (
+        let i = Number(item.startDay.getDate());
+        i <= Number(item.endDay.getDate());
+        i++
+      ) {
+        for (let j = 0; j < 3; j++) {
+          if (CountArr[i][j] === false) {
+            CountArr[i][j] = true;
+
+            console.log('start :', i);
+            console.log('index :', j);
+            break;
+          }
+        }
+      }
+
+      return (
+        <StEventBlock top={topPercent} width={widthPercent} left={leftPercent}>
+          {item.title}
+        </StEventBlock>
+      );
+    });
+
+    return eventArr;
+  }, [selectedYear, selectedMonth, dateTotalCount]);
+
   return (
-    <StContainer>
-      <StTitle>
-        <StTitleDateBlock>
-          <Styear>
-            <div>{today.year.toString().split('').splice(0, 2)}</div>
-          </Styear>
-          <StMonth>
-            <div style={{ marginRight: '10px' }}>
-              {selectedYear.toString().split('').splice(2, 4)}
-            </div>
-            /{selectedMonth.toString().padStart(2, '0')}
-          </StMonth>
-        </StTitleDateBlock>
-        <Stpagination>
-          <button onClick={preMonth}>◀︎</button>
-          <button onClick={nextMonth}>▶︎</button>
-        </Stpagination>
-      </StTitle>
+    <StContainer width={width}>
       <StWeekBlock>{returnWeek()}</StWeekBlock>
-      <StDateBlcok>{returnDay()}</StDateBlcok>
+      <StLine />
+      <div style={{ position: 'relative' }}>
+        <StDateBlcok>{returnDay()}</StDateBlcok>
+        <StEventContainer>{returnEvent()}</StEventContainer>
+      </div>
     </StContainer>
   );
 };
