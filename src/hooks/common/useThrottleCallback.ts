4160cf0e257317b34735d48d3ed431b13fcd6595
SsongCh94 <thddkwl94@gmail.com>
2023-04-25 19:41:21 +0900

    [FEAT] : ⚙️ useThrottleCallback TS화

--- src/hooks/common/useThrottleCallback.ts
+++ src/hooks/common/useThrottleCallback.ts
@@ -7,15 +7,14 @@ export default function useThrottleCallback(
   delay: number,
   eventType: 'wheel' | 'scroll'
 ) {
-  const throttleRef = useRef<any>(null);
+  const throttleRef = useRef<number | null>(null);
 
   const throttledCallback = useCallback(
     (e: Event) => {
       if (!throttleRef.current) {
-        throttleRef.current = true;
         callback(e);
-        setTimeout(() => {
-          throttleRef.current = false;
+        throttleRef.current = window.setTimeout(() => {
+          throttleRef.current = null;
         }, delay);
       }
     },
@@ -27,58 +26,14 @@ export default function useThrottleCallback(
       window.addEventListener(eventType, throttledCallback, { passive: false });
       return () => {
         window.removeEventListener(eventType, throttledCallback);
-        clearTimeout(throttleRef.current);
+        if (throttleRef.current !== null) clearTimeout(throttleRef.current);
       };
     } else {
       return () => {
-        clearTimeout(throttleRef.current);
+        if (throttleRef.current !== null) clearTimeout(throttleRef.current);
       };
     }
   }, [eventType, throttledCallback]);
 
   return throttledCallback;
 }
-
-// import { useCallback, useEffect, useRef } from 'react';
-
-// type ThrottleCallback = (e: Event) => void;
-
-// export default function useThrottleCallback(
-//   callback: ThrottleCallback,
-//   delay: number,
-//   eventType: 'wheel' | 'scroll'
-// ) {
-//   const throttleRef = useRef<number | null>(null);
-
-//   const throttledCallback = useCallback(
-//     (e: Event) => {
-//       if (!throttleRef.current) {
-//         callback(e);
-//         throttleRef.current = window.setTimeout(() => {
-//           throttleRef.current = null;
-//         }, delay);
-//       }
-//     },
-//     [callback, delay]
-//   );
-
-//   useEffect(() => {
-//     if (eventType) {
-//       window.addEventListener(eventType, throttledCallback, { passive: false });
-//       return () => {
-//         window.removeEventListener(eventType, throttledCallback);
-//         if (throttleRef.current !== null) {
-//           clearTimeout(throttleRef.current);
-//         }
-//       };
-//     } else {
-//       return () => {
-//         if (throttleRef.current !== null) {
-//           clearTimeout(throttleRef.current);
-//         }
-//       };
-//     }
-//   }, [eventType, throttledCallback]);
-
-//   return throttledCallback;
-// }
