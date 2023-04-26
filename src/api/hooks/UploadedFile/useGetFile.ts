58882d46d21b23388a28e812e5baee907feb3ace
SsongCh94 <thddkwl94@gmail.com>
2023-04-26 14:15:55 +0900

    [!BREAKING CHANGE] : 🚧 UploadedFileTab API 수정

--- src/api/hooks/UploadedFile/useGetFile.ts
+++ src/api/hooks/UploadedFile/useGetFile.ts
@@ -2,19 +2,10 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 import apis from '../../axios/api';
 import { keys } from '../../utils/createQueryKey';
 import { AxiosError } from 'axios';
-
-interface File {
-  enrollDay: string;
-  eventId: number;
-  file: string;
-  fileName: string;
-  title: string;
-  userName: string;
-  eventType?: string;
-}
+import { UploadedFileList } from '../../../components/UploadedFileTab/interfaces';
 
 interface PageData {
-  [x: string]: File[] | number;
+  [x: string]: UploadedFileList[] | number;
 }
 
 export const useGetFile = (type: 'myfiles' | 'meetingfiles' | 'reportfiles') => {
@@ -26,6 +17,7 @@ export const useGetFile = (type: 'myfiles' | 'meetingfiles' | 'reportfiles') => 
     queryKey: [keys.GET_REPORT_FILE, type], // 쿼리 키
     queryFn: async ({ pageParam = 1 }) => {
       const response = await apis.get(`/${type}?pageSize=6&pageNum=${pageParam}`);
+      console.log(response.data);
       return { [type]: response.data[type], pageNum: pageParam };
     }, // API 호출 함수
     getNextPageParam: lastPage => {
@@ -33,7 +25,9 @@ export const useGetFile = (type: 'myfiles' | 'meetingfiles' | 'reportfiles') => 
       const nextPageNum = (lastPage.pageNum as number) + 1;
 
       // 만약 lastPage의 데이터가 비어 있다면, 'undefined'를 반환하여 다음 페이지를 요청하지 않도록 합니다.
-      return lastPage && lastPage[type] && (lastPage[type] as File[]).length > 0
+      return lastPage &&
+        lastPage[type] &&
+        (lastPage[type] as UploadedFileList[]).length > 0
         ? nextPageNum
         : undefined;
     },
