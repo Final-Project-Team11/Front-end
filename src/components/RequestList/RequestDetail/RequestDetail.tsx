03dbb139fb5a5ec057098d2569fb8abdbafd939a
SsongCh94 <thddkwl94@gmail.com>
2023-04-26 18:21:18 +0900

    [FEAT] : ⚙️ RequestDetail 선택 sweetAlert 적용

--- src/components/RequestList/RequestDetail/RequestDetail.tsx
+++ src/components/RequestList/RequestDetail/RequestDetail.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import * as UI from './style';
-import { DecideParams, DetailProps } from './interfaces';
+import { DecideParams, DetailProps } from '../interfaces';
 import { useDecideRequest } from '../../../api/hooks/Request/useDecideRequest';
 import Swal from 'sweetalert2';
 import { COLOR } from '../../../styles/colors';
 
 const RequestDetail = ({ data, isLoading, closeModal }: DetailProps) => {
-  if (isLoading) {
+  if (isLoading || !data) {
     return <div>Loading....</div>;
   }
 
   // 수락 거절 버튼 핸들러
   const { decideRequest } = useDecideRequest();
   const acceptBtnClickHandler = (params: DecideParams) => {
-    // decideRequest(params);
-
     let message: string;
-    if (params.types === 'accept' ? (message = '수락') : (message = '거절'))
+    if (params.types === 'accept' ? (message = '수락') : (message = '거절')) {
+      const sweetAlertDiv = document.getElementById('sweetAlertDiv');
+      if (!sweetAlertDiv) return;
+
       Swal.fire({
         title: `${message}하시겠습니까?`,
         text: "You won't be able to revert this!",
@@ -25,15 +26,14 @@ const RequestDetail = ({ data, isLoading, closeModal }: DetailProps) => {
         confirmButtonColor: COLOR.PAGE_BLUE,
         cancelButtonColor: COLOR.VACATION_RED,
         confirmButtonText: message,
+        target: sweetAlertDiv, // 여기에 target 속성을 추가
         customClass: {
           popup: 'swal-custom-z-index',
         },
-        // 직접 style 속성 추가
         didOpen: () => {
           const popup = document.querySelector('.swal-custom-z-index');
           if (popup) {
-            // 타입을 강제로 HTMLElement로 캐스팅
-            (popup as HTMLElement).style.zIndex = '2500'; // 기존 모달의 z-index보다 높은 값을 설정하세요
+            (popup as HTMLElement).style.zIndex = '2500';
           }
         },
       }).then(result => {
@@ -42,65 +42,69 @@ const RequestDetail = ({ data, isLoading, closeModal }: DetailProps) => {
           Swal.fire(`${message}되었습니다.`, 'success');
         }
       });
+    }
   };
 
   const acceptParam = {
-    eventId: data.eventId,
+    eventId: data.Id,
     types: 'accept',
   };
   const declineParam = {
-    eventId: data.eventId,
+    eventId: data.Id,
     types: 'deny',
   };
 
   return (
-    <UI.Modal>
-      <UI.Header>
-        <UI.HeaderIcon />
-        <UI.TitleSpan>
-          {data.start === data.end ? data.start : `${data.start} ~ ${data.end}`}
-        </UI.TitleSpan>
-        <UI.TitleSpan>{data.userName}</UI.TitleSpan>
-        <UI.TitleSpan>{data.title}</UI.TitleSpan>
-        <UI.DecideButton
-          types="accept"
-          onClick={() => acceptBtnClickHandler(acceptParam)}
-        >
-          수락
-        </UI.DecideButton>
-        <UI.DecideButton
-          types="decline"
-          onClick={() => acceptBtnClickHandler(declineParam)}
-        >
-          닫기
-        </UI.DecideButton>
-        <UI.Devider positions="Header" />
-      </UI.Header>
-      <UI.ContentArea>
-        <UI.ContentSpan>{data.body}</UI.ContentSpan>
-      </UI.ContentArea>
-      <UI.Footer>
-        <UI.FooterHalf>
-          {data.files.map((file, idx) => {
-            if (file.fileName && file.fileLocation) {
-              return (
-                <React.Fragment key={idx}>
-                  <UI.FooterSpanBlock>{file.fileName}</UI.FooterSpanBlock>
-                  <a href={file.fileLocation}>{file.fileName}</a>
-                </React.Fragment>
-              );
-            }
-            return null;
-          })}
-        </UI.FooterHalf>
-        <UI.Devider positions="Footer" />
-        <UI.FooterHalf>
-          {data.attendees.map((tag, idx) => {
-            return <UI.FooterSpanBlock key={idx}>@ {tag}</UI.FooterSpanBlock>;
-          })}
-        </UI.FooterHalf>
-      </UI.Footer>
-    </UI.Modal>
+    <>
+      <UI.Modal>
+        <UI.Header>
+          <UI.HeaderIcon />
+          <UI.TitleSpan>
+            {data.start === data.end ? data.start : `${data.start} ~ ${data.end}`}
+          </UI.TitleSpan>
+          <UI.TitleSpan>{data.userName}</UI.TitleSpan>
+          <UI.TitleSpan>{data.title}</UI.TitleSpan>
+          <UI.DecideButton
+            types="accept"
+            onClick={() => acceptBtnClickHandler(acceptParam)}
+          >
+            수락
+          </UI.DecideButton>
+          <UI.DecideButton
+            types="decline"
+            onClick={() => acceptBtnClickHandler(declineParam)}
+          >
+            닫기
+          </UI.DecideButton>
+          <UI.Devider positions="Header" />
+        </UI.Header>
+        <UI.ContentArea>
+          <UI.ContentSpan>{data.body}</UI.ContentSpan>
+        </UI.ContentArea>
+        <UI.Footer>
+          <UI.FooterHalf>
+            {data.files.map((file, idx) => {
+              if (file.fileName && file.fileLocation) {
+                return (
+                  <React.Fragment key={idx}>
+                    <UI.FooterSpanBlock>{file.fileName}</UI.FooterSpanBlock>
+                    <a href={file.fileLocation}>{file.fileName}</a>
+                  </React.Fragment>
+                );
+              }
+              return null;
+            })}
+          </UI.FooterHalf>
+          <UI.Devider positions="Footer" />
+          <UI.FooterHalf>
+            {data.attendees.map((tag, idx) => {
+              return <UI.FooterSpanBlock key={idx}>@ {tag}</UI.FooterSpanBlock>;
+            })}
+          </UI.FooterHalf>
+        </UI.Footer>
+      </UI.Modal>
+      <div id="sweetAlertDiv" />
+    </>
   );
 };
 
