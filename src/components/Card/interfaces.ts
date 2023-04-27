5b6ca943f187958bab58e5fcff1df9ef38d18af0
SsongCh94 <thddkwl94@gmail.com>
2023-04-27 18:32:50 +0900

    [FEAT] : ⚙️ interface 수정, 추가작업

---
+++ src/components/Card/interfaces.ts
@@ -0,0 +1,11 @@
+export interface CardProps {
+  tab?: boolean;
+}
+
+export interface CardDetail {
+  userId: string;
+  userName: string;
+  profileImg: string;
+  birthDay: string;
+  phoneNum: string;
+}
