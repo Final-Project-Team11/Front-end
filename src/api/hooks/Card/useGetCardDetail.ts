d80bd5abdc3020395cfa87127cbe119b56a9bbd9
SsongCh94 <thddkwl94@gmail.com>
2023-04-27 23:36:34 +0900

    [FEAT] : ⚙️ query요청 타입 추가

--- src/api/hooks/Card/useGetCardDetail.ts
+++ src/api/hooks/Card/useGetCardDetail.ts
@@ -1,12 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import { keys } from '../../utils/createQueryKey';
 import apis from '../../axios/api';
+import { CardInfoType } from '../../../components/Card/interfaces';
 
 export const useGetCardDetail = () => {
-  const { data, refetch, isLoading } = useQuery({
+  const { data, refetch, isLoading } = useQuery<CardInfoType>({
     queryKey: [keys.GET_CARD_DETAIL],
     queryFn: async () => {
-      const response = await apis.get(`/userInfo/profile`);
+      const response = await apis.get(`/usersInfo/profile`);
       console.log(response);
       return response.data;
     },
