172a9ebeca3fc8092fc983360411ce29df6f8892
ChanwooPark <chanw9503@gmail.com>
2023-04-28 20:53:42 +0900

    [FEAT] : ⚙️ 회의보고서 용 API 추가

---
+++ src/api/hooks/Main/usePostMeetingReport.ts
@@ -0,0 +1,43 @@
+import { useMutation } from '@tanstack/react-query';
+import React from 'react';
+import apis from '../../axios/api';
+
+interface Paylaod {
+  postInfo?: {
+    title?: string;
+    body?: string;
+    start?: Date;
+    end?: Date;
+    attendees?: string[];
+    fileList?: File[] | undefined;
+  };
+  id: string | number | undefined;
+}
+
+const usePostMeetingReport = () => {
+  const mutation = useMutation({
+    mutationFn: async (payload: Paylaod) => {
+      const formData = new FormData();
+      const start = payload.postInfo?.start?.toString();
+      const end = payload.postInfo?.end?.toString();
+
+      payload.postInfo?.fileList?.map((item, index) => formData.append('file', item));
+      formData.append('start', start || '');
+      formData.append('end', end || '');
+      formData.append('title', payload.postInfo?.title || '');
+      formData.append('body', payload.postInfo?.body || '');
+      payload.postInfo?.attendees?.map((item, index) =>
+        formData.append(`attendees[${index}]`, item)
+      );
+
+      const data = await apis.post(`meeting/${payload.id}`, formData);
+      return data.data;
+    },
+    onSuccess: config => {
+      console.log('config', config);
+    },
+  });
+  return mutation;
+};
+
+export default usePostMeetingReport;
