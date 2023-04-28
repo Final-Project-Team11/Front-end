1f84afac3970ee92b9b161a22e013917cf1f3090
choidami5126 <dahyun5003@kakao.com>
2023-04-28 11:05:40 +0900

    [!HOTFIX] : 💥forwardRef 사용해 Input 컴포넌트에 ref 전달

--- src/components/Atoms/Input/CustomInput.tsx
+++ src/components/Atoms/Input/CustomInput.tsx
@@ -12,26 +12,20 @@ export interface InputProps {
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const CustomInput = ({
-  style,
-  name,
-  type,
-  value,
-  inputType,
-  placeholder,
-  onChange,
-}: InputProps) => {
-  return (
-    <StInput
-      inputType={inputType}
-      type={type}
-      value={value}
-      onChange={onChange}
-      placeholder={placeholder}
-      name={name}
-      style={style}
-    />
-  );
-};
-
+const CustomInput = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ style, name, type, value, inputType, placeholder, onChange }: InputProps, ref) => {
+    return (
+      <StInput
+        ref={ref}
+        inputType={inputType}
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        name={name}
+        style={style}
+      />
+    );
+  }
+);
 export default CustomInput;
