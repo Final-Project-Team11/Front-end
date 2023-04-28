259bb78ec89275a5023c87055856015e4e53d546
ChanwooPark <chanw9503@gmail.com>
2023-04-28 12:49:55 +0900

    [DESIGN] : 🎨 Dropdown position 적용

--- src/components/Dropdown/styles.ts
+++ src/components/Dropdown/styles.ts
@@ -1,5 +1,6 @@
 import styled, { css } from 'styled-components';
 import * as interfaces from './interfaces';
+import { UlStyleProps } from '../Main/DocumentForm/components/HashTag/interfaces';
 
 const sizes: interfaces.Sizes = {
   small: {
@@ -29,6 +30,7 @@ export const StBlock = styled.div<interfaces.CssProps>`
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
+  gap: 5px;
 `;
 
 export const StLi = styled.li<interfaces.CssProps>`
@@ -46,4 +48,20 @@ export const StLi = styled.li<interfaces.CssProps>`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  opacity: 1;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
+export const StUlBlock = styled.ul<UlStyleProps>`
+  position: absolute;
+  z-index: 100;
+  background-color: white;
+  /* border: 1px solid black; */
+  box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 2px 0px;
+  border-top: none;
+  ${({ pos }) =>
+    `top : ${pos.top + pos.height + 2}px; left :${pos.left}px; width:${pos.width - 1}px;`}
 `;
