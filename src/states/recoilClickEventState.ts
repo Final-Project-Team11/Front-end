ffc93fa3cdbaf0a5043ecfcc4af53817cc460cf5
ChanwooPark <chanw9503@gmail.com>
2023-04-28 21:07:59 +0900

    [REFACTOR] : ♻️  CalendarProps interface 분리

--- src/states/recoilClickEventState.ts
+++ src/states/recoilClickEventState.ts
@@ -1,31 +1,5 @@
 import { atom } from 'recoil';
-interface CalendarProps {
-  id?: string | number;
-  Id?: string | number;
-  calendarId?: string;
-  title?: string;
-  body?: string;
-  start?: Date;
-  end?: Date;
-  attendees?: string[];
-  location?: string;
-  userId?: string;
-  userName?: string;
-  typeDetail?: string;
-  files?: [
-    {
-      fileLocation?: string;
-      fileName?: string;
-    }
-  ];
-
-  fileList?: File[] | undefined; //서버에서 정보 받을 때,
-  color?: string;
-  backgroundColor?: string;
-  dragBackgroundColor?: string;
-  borderColor?: string;
-  isReadOnly?: boolean;
-}
+import { CalendarProps } from '../pages/SubMain/interfaces';
 
 export const recoilClickEventState = atom<CalendarProps>({
   key: 'eventClick',
