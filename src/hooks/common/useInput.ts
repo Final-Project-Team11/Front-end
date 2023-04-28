8d57273051a79be8cc3a32a763b2aee6cdc0083e
SsongCh94 <thddkwl94@gmail.com>
2023-04-28 16:10:50 +0900

    [FEAT] : ⚙️ useInput 커스텀훅 유효성 검사 추가

--- src/hooks/common/useInput.ts
+++ src/hooks/common/useInput.ts
@@ -1,10 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export function useInput(
-  maxLength?: number,
-  initialValue?: string
-): [string, (e: React.ChangeEvent<HTMLInputElement>) => void, (value: string) => void] {
+  maxLength?: number, //maxLength
+  initialValue?: string, // initialValue
+  valid?: RegExp // 유효성검사 RegExp는 정규식의 타입
+): [
+  string,
+  (e: React.ChangeEvent<HTMLInputElement>) => void,
+  (value: string) => void,
+  boolean | undefined
+] {
   const [inputValue, setInputValue] = useState<string>(initialValue || '');
+
+  useEffect(() => {
+    setInputValue(initialValue || '');
+  }, [initialValue]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (maxLength) {
@@ -16,7 +26,13 @@ export function useInput(
     }
   };
 
-  return [inputValue, handleChange, setInputValue];
+  // 유효성 검사 => inputValue 를 정규식 valid로 검사해서 isValid boolean을 뱉어냄
+  let isValid: boolean | undefined;
+  if (valid) {
+    isValid = valid.test(inputValue);
+  }
+
+  return [inputValue, handleChange, setInputValue, isValid];
 }
 
 export default useInput;
