f861835f719d841466a3e48ea6df636babea6923
ChanwooPark <chanw9503@gmail.com>
2023-04-30 21:37:08 +0900

    [REFACTOR] : ♻️ 성능 최적화를 위한 코드분할 (react.lazy) 사용

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,29 +1,30 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from '../pages/Home/Home';
-import Login from '../pages/Login/Login';
-import MasterSignup from '../pages/MasterSignup/MasterSignup';
-import Main from '../pages/Main/Main';
-import MyPage from '../pages/MyPage/MyPage';
-import Manager from '../pages/Manager/Manager';
-import Business from '../pages/Business/Business';
-import Write from '../pages/Write/Write';
-import { SubMain } from '../pages/SubMain/SubMain';
-import NotFound from '../pages/NotFound/NotFound';
+import { Suspense, lazy } from 'react';
+
+const Main = lazy(() => import('../pages/Main/Main'));
+const Home = lazy(() => import('../pages/Home/Home'));
+const Login = lazy(() => import('../pages/Login/Login'));
+const MasterSignup = lazy(() => import('../pages/MasterSignup/MasterSignup'));
+const MyPage = lazy(() => import('../pages/MyPage/MyPage'));
+const Manager = lazy(() => import('../pages/Manager/Manager'));
+const Business = lazy(() => import('../pages/Business/Business'));
+const NotFound = lazy(() => import('../pages/NotFound/NotFound'));
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/masterSignup" element={<MasterSignup />} />
-        <Route path="/main" element={<Main />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/manager" element={<Manager />} />
-        <Route path="/business" element={<Business />} />
-        <Route path="/write" element={<Write />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/masterSignup" element={<MasterSignup />} />
+          <Route path="/main" element={<Main />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/manager" element={<Manager />} />
+          <Route path="/business" element={<Business />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
