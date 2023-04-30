accb27bfa5bd61c73b4eea94b1a284634f651e39
ChanwooPark <chanw9503@gmail.com>
2023-04-30 21:34:02 +0900

    [FIX] : 🔧 이미지 반복 방지 추가

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -6,7 +6,9 @@ import { useNavigate } from 'react-router-dom';
 const NotFound = () => {
   const navigate = useNavigate();
   return (
-    <StBlock style={{ backgroundImage: `url(${notFound})` }}>
+    <StBlock
+      style={{ backgroundImage: `url(${notFound})`, backgroundRepeat: 'no-repeat' }}
+    >
       <StButton onClick={() => navigate(-1)}>이전 페이지로</StButton>
     </StBlock>
   );
