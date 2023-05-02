25705cdb4185f5dfe9c0fe0a20bb3b006b51ee02
choidami5126 <dahyun5003@kakao.com>
2023-05-02 17:38:26 +0900

    [REFACTOR] : ♻️  UT 피드백을 반영한 리팩토링

--- src/pages/Business/components/CreateUser.tsx
+++ src/pages/Business/components/CreateUser.tsx
@@ -8,6 +8,8 @@ import { useForm } from 'react-hook-form';
 import Wrapper_Row from '../../../components/Atoms/Wrapper_Row/Wrapper_Row';
 import Swal from 'sweetalert2';
 import { useSignup } from '../hooks/useSignup';
+import { StFrom, StH1, VaildP } from '../styles';
+import Wrapper_Column from '../../../components/Atoms/Wrapper_Column/Wrapper_Column';
 
 type UserSignupInfoPlus = {
   userName: string;
@@ -21,26 +23,33 @@ type UserSignupInfoPlus = {
 };
 
 const CreateUser = () => {
-  // <------------------------------React-Hook-Form------------------------------>
-  const { register, handleSubmit, getValues, reset } = useForm<UserSignupInfoPlus>();
+  // <-----------------------------------React-Hook-Form----------------------------------->
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    reset,
+    formState: { errors },
+  } = useForm<UserSignupInfoPlus>({ mode: 'onChange' });
 
-  // <-------------------------아이디 유호성 검사, 중복확인------------------------->
-  const { validUserId, checkUserId, userIdValidation } = useUserIdValidation();
+  // <------------------------------아이디 유호성 검사, 중복확인------------------------------>
+  const { validUserId, checkUserId, userIdValidation, setUserIdValidation } =
+    useUserIdValidation();
 
-  const checkUserIdHandler = () => {
+  const checkUserIdHandler = async () => {
     const { userId } = getValues();
-    validUserId(userId);
-    if (userIdValidation) {
+    const isVaild = await validUserId(userId);
+    if (isVaild) {
       checkUserId.mutate(userId);
     } else {
       Swal.fire({
         icon: 'error',
         title: '유효하지 않은 아이디 입니다.',
-        text: '아이디 양식을 다시 재확인 해주세요.',
+        text: '영문과 숫자를 조합해 5자 이상 입력해 주세요',
       });
     }
   };
-  // <------------------------------권한------------------------------>
+  // <-----------------------------------권한----------------------------------->
   type Auth = {
     auth: number | string;
   };
@@ -54,8 +63,7 @@ const CreateUser = () => {
     { name: '관리자', value: 2 },
     { name: '직원', value: 3 },
   ];
-
-  // <------------------------------유저 생성------------------------------>
+  // <-----------------------------------유저 생성----------------------------------->
   const { signup } = useSignup();
   const submitSignInfoHandler = (data: UserSignupInfoPlus) => {
     const joinDay = new Date(data.joinDay);
@@ -71,112 +79,117 @@ const CreateUser = () => {
     };
     if (userIdValidation) {
       signup.mutate(newData);
+      setUserIdValidation(false);
       reset();
     } else {
-      alert('가입에 실패하였습니다 입력한 내용을 확인해주세요');
+      Swal.fire({
+        icon: 'error',
+        title: '아이디 중복 검사를 진행해주세요.',
+      });
     }
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(submitSignInfoHandler)}
-      style={{
-        width: '500px',
-        margin: '50px 210px 50px 210px',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '30px',
-      }}
-    >
-      <h1
-        style={{
-          width: '500px',
-          fontSize: '18px',
-          fontWeight: 'bolder',
-          borderBottom: '1px solid black',
-          paddingBottom: '15px',
-          boxSizing: 'border-box',
-          marginBottom: '25px',
-        }}
-      >
-        유저 생성
-      </h1>
-      {/* <------------------------이름------------------------> */}
+    <StFrom onSubmit={handleSubmit(submitSignInfoHandler)}>
+      <StH1 style={{ width: '500px' }}>유저 생성</StH1>
+      {/* <------------------------------이름-----------------------------> */}
       <CustomLabel>
         이름
         <CustomInput
-          inputType="cUser"
+          inputType="login"
+          style={{ width: '500px' }}
           placeholder="직원의 이름을 입력해주세요"
           {...register('userName', {
-            required: true,
+            required: '필수 입력값 입니다',
           })}
         />
       </CustomLabel>
-      {/* <------------------------부서&팀------------------------> */}
-      <div style={{ display: 'flex', flexDirection: 'row', gap: '20px' }}>
-        <CustomLabel>
-          부서
-          <CustomInput inputType="cUserHalf" placeholder="준비 중인 입력칸입니다." />
-        </CustomLabel>
-        <CustomLabel>
-          팀
-          <CustomInput
-            inputType="cUserHalf"
-            placeholder="직원의 팀을 입력해주세요"
-            {...register('team', {
-              required: true,
-            })}
-          />
-        </CustomLabel>
-      </div>
-      {/* <------------------------직급&직무------------------------> */}
-      <div style={{ display: 'flex', flexDirection: 'row', gap: '20px' }}>
-        <CustomLabel>
-          직급
-          <CustomInput
-            inputType="cUserHalf"
-            placeholder="직원의 직급을 입력해주세요"
-            {...register('rank', {
-              required: true,
-            })}
-          />
-        </CustomLabel>
-        <CustomLabel>
-          직무
-          <CustomInput
-            inputType="cUserHalf"
-            placeholder="직원의 직무를 입력해주세요"
-            {...register('job', {
-              required: true,
-            })}
-          />
-        </CustomLabel>
-      </div>
+      {errors.userName && <VaildP>{errors.userName.message}</VaildP>}
+      {/* <----------------------------------부서----------------------------------> */}
+      <CustomLabel>
+        팀
+        <CustomInput
+          inputType="login"
+          style={{ width: '500px' }}
+          placeholder="직원의 팀을 입력해주세요"
+          {...register('team', {
+            required: '필수 입력값 입니다',
+          })}
+        />
+      </CustomLabel>
+      {errors.team && <VaildP>{errors.team.message}</VaildP>}
+      {/* <-----------------------------직급&직무-----------------------------> */}
+      <Wrapper_Row style={{ gap: '20px' }}>
+        <Wrapper_Column>
+          <CustomLabel>
+            직급
+            <CustomInput
+              inputType="login"
+              style={{ width: '240px' }}
+              placeholder="직원의 직급을 입력해주세요"
+              {...register('rank', {
+                required: '필수 입력값 입니다',
+              })}
+            />
+          </CustomLabel>
+          {errors.rank && (
+            <VaildP style={{ margin: '15px 0' }}>{errors.rank.message}</VaildP>
+          )}
+        </Wrapper_Column>
+        <Wrapper_Column>
+          <CustomLabel>
+            직무
+            <CustomInput
+              inputType="login"
+              style={{ width: '240px' }}
+              placeholder="직원의 직무를 입력해주세요"
+              {...register('job', {
+                required: '필수 입력값 입니다',
+              })}
+            />
+          </CustomLabel>
+          {errors.job && (
+            <VaildP style={{ margin: '15px 0' }}>{errors.job.message}</VaildP>
+          )}
+        </Wrapper_Column>
+      </Wrapper_Row>
       {/* <------------------------월급일&입사일------------------------> */}
-      <div style={{ display: 'flex', flexDirection: 'row', gap: '20px' }}>
-        <CustomLabel>
-          월급일
-          <CustomInput
-            inputType="cUserHalf"
-            placeholder="직원의 월급일을 입력해주세요"
-            maxLength={2}
-            {...register('salaryDay', {
-              required: true,
-            })}
-          />
-        </CustomLabel>
-        <CustomLabel>
-          입사일
-          <CustomInput
-            inputType="cUserHalf"
-            type="date"
-            placeholder="직원의 입사일을 입력해주세요"
-            {...register('joinDay', {
-              required: true,
-            })}
-          />
-        </CustomLabel>
-      </div>
+      <Wrapper_Row style={{ gap: '20px' }}>
+        <Wrapper_Column>
+          <CustomLabel>
+            월급일
+            <CustomInput
+              inputType="login"
+              style={{ width: '240px' }}
+              placeholder="직원의 월급일을 입력해주세요"
+              maxLength={2}
+              {...register('salaryDay', {
+                required: '필수 입력값 입니다',
+              })}
+            />
+          </CustomLabel>
+          {errors.salaryDay && (
+            <VaildP style={{ margin: '15px 0 -15px' }}>{errors.salaryDay.message}</VaildP>
+          )}
+        </Wrapper_Column>
+        <Wrapper_Column>
+          <CustomLabel>
+            입사일
+            <CustomInput
+              inputType="login"
+              style={{ width: '240px' }}
+              type="date"
+              placeholder="직원의 입사일을 입력해주세요"
+              {...register('joinDay', {
+                required: '필수 입력값 입니다',
+              })}
+            />
+          </CustomLabel>
+          {errors.joinDay && (
+            <VaildP style={{ margin: '15px 0 -15px' }}>{errors.joinDay.message}</VaildP>
+          )}
+        </Wrapper_Column>
+      </Wrapper_Row>
       {/* <------------------------권한------------------------> */}
       <Dropdown
         size="small"
@@ -192,20 +205,24 @@ const CreateUser = () => {
           padding: '15px',
           fontWeight: 'bold',
           color: '#484240',
-          marginBottom: '-30px',
         }}
       >
-        권한
+        권한&nbsp;(필수)
       </Dropdown>
       {/* <-----------------------------아이디-----------------------------> */}
-      <Wrapper_Row style={{ alignItems: 'center', gap: '20px' }}>
+      <Wrapper_Row style={{ alignItems: 'center', gap: '20px', marginTop: '-30px' }}>
         <CustomLabel>
           아이디
           <CustomInput
-            inputType="cUserId"
-            placeholder="영 대, 소문자, 숫자 5자 이상 입력해주세요."
+            inputType="login"
+            style={{ width: '336px' }}
+            placeholder="영문과 숫자를 조합해 5자 이상 입력해 주세요"
             {...register('userId', {
-              required: true,
+              required: '필수 입력값 입니다',
+              pattern: {
+                value: /^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{5,}$/,
+                message: '영문과 숫자를 조합해 5자 이상 입력해 주세요',
+              },
             })}
           />
         </CustomLabel>
@@ -218,10 +235,11 @@ const CreateUser = () => {
           중복 확인
         </CustomButton>
       </Wrapper_Row>
+      {errors.userId && <VaildP>{errors.userId.message}</VaildP>}
       <div style={{ display: 'flex', justifyContent: 'center', marginTop: '68px' }}>
-        <CustomButton buttonType="cUserSubmit">유저 생성</CustomButton>
+        <CustomButton buttonType="blackBackground">생성</CustomButton>
       </div>
-    </form>
+    </StFrom>
   );
 };
 export default CreateUser;
