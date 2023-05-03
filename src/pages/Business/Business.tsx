6da8169059a10a57a98a5d293dc64b700f4decb3
choidami5126 <dahyun5003@kakao.com>
2023-05-03 10:29:47 +0900

    [FEAT] : ⚙️ authLevel에 따른 입장 제한 설정

--- src/pages/Business/Business.tsx
+++ src/pages/Business/Business.tsx
@@ -1,52 +1,43 @@
-import React from 'react';
-import Dropdown from '../../components/Atoms/Dropdown/Dropdown';
-import 'react-datepicker/dist/react-datepicker.css';
-import { Users, useGetUser } from './hooks/useGetUser';
-import { useDeleteUser } from './hooks/useDeletUser';
-import { usePatchUser } from './hooks/usePatchUser';
-import { Wrapper, ViewUserArea, StSpan, UserInfo } from './styles';
+import { useGetUser } from './hooks/useGetUser';
+import { Wrapper, ViewUserArea } from './styles';
 import CreateUser from './components/CreateUser';
 import Swal from 'sweetalert2';
-import CustomModal from '../../components/Atoms/Modal/CustomModal';
-import CustomLabel from '../../components/Atoms/Label/CustomLabel';
-import CustomInput from '../../components/Atoms/Input/CustomInput';
-import CustomButton from '../../components/Atoms/Button/CustomButton';
-import Wrapper_Row from '../../components/Atoms/Wrapper_Row/Wrapper_Row';
+
 import ViewUser from './components/ViewUser';
+import { getCookie } from '../../api/auth/CookieUtils';
+import jwtDecode, { JwtPayload } from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
 
 const Business = () => {
-  // 모달 상태변수, 콜백함수
-  const [showModal, setShowModal] = React.useState<boolean>(false);
-
   const { data, isLoading } = useGetUser();
-
-  const selecteAuthorityPatchHandler = (value: number | string) => {
-    setPatchUserInfo({ ...patchUserInfo, authLevel: Number(value) });
-  };
+  const navigate = useNavigate();
 
   if (!data && isLoading) {
     <div>Loading</div>;
   }
 
-  interface PatchUserInfo {
-    team: string;
-    authLevel: number | string;
-    rank: string;
-    job: string;
+  const token = getCookie('token');
+
+  if (!token) {
+    Swal.fire({
+      icon: 'warning',
+      title: '비정상적인 접근입니다',
+    });
+    navigate('/');
   }
+  const decoded = token && jwtDecode<JwtPayload>(token);
+  const authLevel = decoded ? decoded.authLevel : '';
 
-  const [patchUserInfo, setPatchUserInfo] = React.useState<PatchUserInfo>({
-    team: '',
-    authLevel: '',
-    rank: '',
-    job: '',
-  });
-
-  // 권한 드롭 다운 배열
+  if (authLevel === 2 || authLevel === 3) {
+    Swal.fire({
+      icon: 'warning',
+      title: '권한이 없습니다',
+    });
+    navigate('/main');
+  }
 
   return (
     <Wrapper>
-      {/* <------------------------------유저 조회------------------------------> */}
       <ViewUserArea>
         <ViewUser />
       </ViewUserArea>
